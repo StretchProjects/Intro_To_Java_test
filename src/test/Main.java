b12472a2e0886d66e996dda059eda4e812a60353
Stretch Projects <StretchProjects@gmail.com>
2014-10-10 20:17:13 +0100

    Files to accompany Youtube series

---
+++ src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {	
+		int number1 = 6;
+		
+		int number2 = 7;
+		
+		int total = number1 + number2;
+		
+		System.out.println(total);
+	}
+
+}
